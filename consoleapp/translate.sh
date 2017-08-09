a164b52952ddf519aafbbf2c0e39e4fd71cac897
Leander Jedamus <ljedamus@web.de>
2017-08-09 20:41:20 +0200

    recreated translate structure

---
+++ consoleapp/translate.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# erzeugt Mittwoch, 09. August 2017 20:33 (C) 2017 von Leander Jedamus
+# modifiziert Mittwoch, 09. August 2017 20:34 von Leander Jedamus
+
+translate.pl --file=main.py --project=consoleapp
+
+# vim:ai sw=2
+
