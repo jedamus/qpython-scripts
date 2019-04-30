b38b3a5d7b9080bc4559c3aab08425ec06bfbe57
Leander Jedamus <ljedamus@web.de>
2019-04-30 14:51:41 +0200

    minor changes

--- sqr.py
+++ sqr.py
@@ -1,6 +1,8 @@
 #-*-coding:utf8;-*-
 #qpy:2
 #qpy:console
+# erzeugt Dienstag, 30. April 2019 13:29 (C) 2019 von Leander Jedamus
+# modifiziert Dienstag, 30. April 2019 14:50 von Leander Jedamus
 
 def sqr(x):
   return(x*x)
