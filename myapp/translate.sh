6ed8d6862e0141c010f804b8c259e108034c9288
Leander Jedamus <ljedamus@web.de>
2019-05-01 01:19:43 +0200

    added --version

--- myapp/translate.sh
+++ myapp/translate.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
 # erzeugt Mittwoch, 09. August 2017 20:33 (C) 2017 von Leander Jedamus
+# modifiziert Mittwoch, 01. Mai 2019 01:18 von Leander Jedamus
 # modifiziert Mittwoch, 09. August 2017 20:34 von Leander Jedamus
 
-translate.pl --file=main.py --project=consoleapp
+translate.pl --file=main.py --project=consoleapp --version=1.4
 
 # vim:ai sw=2
 
