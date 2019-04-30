48c81090806739d577d2190546336da91c34f1b7
Leander Jedamus <ljedamus@web.de>
2019-04-30 14:13:30 +0200

    added datetime.py

---
+++ datetime.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# coding: utf-8
+# ä
+# erzeugt Dienstag, 30. April 2019 13:29 (C) 2019 von Leander Jedamus
+# modifiziert Dienstag, 30. April 2019 13:43 von Leander Jedamus
+
+import datetime
+from get_datum import get_datum
+
+today = datetime.datetime.today()
+datum = today
+days = 4
+day1 = datetime.timedelta (days=1)
+
+import sys
+import locale
+if (sys.platform == "linux2") or (sys.platform == "darwin"):
+  locale.setlocale(locale.LC_ALL, "de_DE.UTF-8")
+
+while datum.weekday() > 4:
+    datum += day1
+for i in range(days):
+    datum += day1
+    if datum.weekday() > 4:
+        datum += day1 + day1
+print(get_datum(datum,"%A, der %d. %B %Y"))
