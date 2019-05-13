1df506e0cf69e290a5176ac6cd7e021ce069e0e2
Leander Jedamus <ljedamus@web.de>
2019-05-13 11:53:27 +0200

    added logging.shutdown()

--- logger2.py
+++ logger2.py
@@ -2,7 +2,7 @@
 #-*-coding:utf8;-*-
 #qpy:2
 #qpy:console
-# modifiziert Montag, 13. Mai 2019 10:52 von Leander Jedamus
+# modifiziert Montag, 13. Mai 2019 11:38 von Leander Jedamus
 
 from __future__ import print_function
 import logging
@@ -28,3 +28,5 @@ if __name__ == '__main__':
   logger.info("dies ist eine Information.")
   logger.debug("debug")
   logging.log(logging.ERROR, "Fehler!")
+
+  logging.shutdown()
