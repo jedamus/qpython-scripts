ced9b05282a7a67a8b488162bf00660ee6d8865f
Leander Jedamus <ljedamus@web.de>
2019-05-13 04:43:17 +0200

    added logger.py

---
+++ logger.py
@@ -0,0 +1,36 @@
+#!/usr/bin/env python
+#-*-coding:utf8;-*-
+#qpy:2
+#qpy:console
+
+from __future__ import print_function
+import logging
+import sys
+import atexit
+
+if sys.platform == "linux4":
+  import os
+  os.chdir("/storage/emulated/0/com.hipipal.qpyplus/scripts")
+
+logger = ""
+atexit.register(logging.shutdown)
+
+if __name__ == '__main__':
+  handler1 = logging.StreamHandler(sys.stdout)
+  handler2 = logging.FileHandler("logger.log","w")
+  
+  frm = logging.Formatter("%(asctime)s %(levelname)s: %(message)s",
+                          "%d.%m.%Y %H:%M:%S")
+  handler1.setFormatter(frm)
+  handler2.setFormatter(frm)
+
+  logger = logging.getLogger()
+  logger.addHandler(handler1)
+  logger.addHandler(handler2)
+  logger.setLevel(logging.DEBUG)
+
+  logger.critical("kritischer Zustand!")
+  logger.warning ("eine Warnung")
+  logger.info("dies ist eine Information.")
+  logger.debug("debug")
+  logging.log(logging.ERROR, "Fehler!")
