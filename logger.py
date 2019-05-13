5a4f19027ad58f8c1794872dc6e81ae91805cae9
Leander Jedamus <ljedamus@web.de>
2019-05-13 13:26:12 +0200

    added gmtime and other optimzations to logger.py

--- logger.py
+++ logger.py
@@ -2,11 +2,12 @@
 #-*-coding:utf8;-*-
 #qpy:2
 #qpy:console
-# modifiziert Montag, 13. Mai 2019 12:51 von Leander Jedamus
+# modifiziert Montag, 13. Mai 2019 13:24 von Leander Jedamus
 
 from __future__ import print_function
 import logging
 import sys
+import time
 import atexit
 
 if sys.platform == "linux4":
@@ -21,11 +22,17 @@ if __name__ == '__main__':
   handler1.setLevel(logging.DEBUG)
   handler2 = logging.FileHandler("logger.log","w")
   handler2.setLevel(logging.DEBUG)
-  
-  frm = logging.Formatter("%(asctime)s %(levelname)s: %(message)s",
-                          "%d.%m.%Y %H:%M:%S")
+ 
+  logging.Formatter.converter=time.gmtime
+  logging._srcFile=None
+  logging.logThreads=0
+  logging.logProcesses=0
+  frm = logging.Formatter("%(asctime)s %(name)s %(levelname)s: %(message)s",
+                          "%d.%m.%Y %H:%M:%S %Z")
   handler1.setFormatter(frm)
+  handler1.setLevel(logging.DEBUG)
   handler2.setFormatter(frm)
+  handler2.setLevel(logging.DEBUG)
 
   logger = logging.getLogger()
   logger.addHandler(handler1)
@@ -35,5 +42,6 @@ if __name__ == '__main__':
   logger.critical("kritischer Zustand!")
   logger.warning ("eine Warnung")
   logger.info("dies ist eine Information.")
-  logger.debug("debug")
+  if logger.isEnabledFor(logging.DEBUG):
+    logger.debug("debug")
   logging.log(logging.ERROR, "Fehler!")
